12622f316592b5eb5c01d53e86fb5c8c140b18a7
tapeoutcutm <tapeout@cutm.ac.in>
2025-08-23 14:58:40 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,58 @@
-`default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
+module tb;
+   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  reg        clk;
+  reg        rst_n;
+  reg        ena;
+  reg [7:0]  ui_in;
   wire [7:0] uo_out;
+
+  reg  [7:0] uio_in = 8'b0;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+  tt_um_vedic_4x4 dut (
+    .clk(clk),
+    .rst_n(rst_n),
+    .ena(ena),
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe)
   );
+
+  always #5 clk = ~clk;
+
+  initial begin
+   
+
+    clk = 0;
+    ena = 1;
+    rst_n = 0;
+    ui_in = 8'b0;
+
+    #20 rst_n = 1;
+
+    ui_in = {4'd3, 4'd2};
+    #20;
+
+    ui_in = {4'd5, 4'd4};
+    #20;
+
+    ui_in = {4'd15, 4'd15};
+    #20;
+
+    ui_in = {4'd9, 4'd0};
+    #20;
+
+   
+  end
 
 endmodule
